d4bf547db5733b5c23f0700df6e18b0c334a4fa7
jeejul1010 <37315537+jeejul1010@users.noreply.github.com>
2022-02-09 16:44:42 +0900

    mst의 간선 갯수는 노드갯수-1

---
+++ MST_baekjoon/MSTMain.cpp
@@ -0,0 +1,7 @@
+#include "SangeunsTravel_9372.h"
+
+int main()
+{
+	sangeunsTravel();
+	return 0;
+}
---
+++ MST_baekjoon/SangeunsTravel_9372.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void sangeunsTravel()
+{
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		int N, M;
+		cin >> N >> M;
+		for (int j = 0; j < M; j++)
+		{
+			int a, b;
+			cin >> a >> b;
+		}
+
+		cout << N - 1 << endl;
+	}
+}
