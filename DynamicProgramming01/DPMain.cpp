92825250008accdf5ab79b843f9023971f808a8d
jeejul1010 <julie0402@gmail.com>
2022-09-22 13:36:16 +0900

    2xn 타일링 문제 dp factorial로 풂 런타임 에러

--- DynamicProgramming01/DPMain.cpp
+++ DynamicProgramming01/DPMain.cpp
@@ -1,9 +1,11 @@
 #include "fibonacci_1003.h"
 #include "FunFunctionExe_9184.h"
+#include "tiling2xn_11726.h"
 
 int main()
 {
 	//fibonacci();
-	funFunctionExe();
+	//funFunctionExe();
+	tiling();
 	return 0;
 }
