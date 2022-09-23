9db907ffd571117ca0a7bc3920e452014e9c31b6
jeejul1010 <julie0402@gmail.com>
2022-09-23 11:59:29 +0900

    dp 점화식 다시 세워서!

--- DynamicProgramming01/tiling2xn_11726.cpp
+++ DynamicProgramming01/tiling2xn_11726.cpp
@@ -2,45 +2,21 @@
 
 using namespace std;
 
-const int maxNum = 1100;
-
-unsigned long long dp[maxNum];
-
-unsigned long long factorial(int num)
-{
-	if (num == 0) return 1;
-	if (num == 1) return 1;
-	if (dp[num] != 0) return dp[num];
-	return dp[num] = num * factorial(num - 1);
-}
-
-unsigned long long comb(int a, int b)
-{
-	return factorial(a) / (factorial(a-b) * factorial(b));
-}
+int dp[1001];
 
 void tiling()
 {
-	int n, a, b;
-	unsigned long long total = 0;
+	int n;
 	cin >> n;
 
-	int i = 0;
+	dp[1] = 1;
+	dp[2] = 2;
 
-	if (n % 2 != 0)
+	for (int i = 3; i <= n; i++)
 	{
-		i = 1;
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
 	}
 
-	while(i<=n)
-	{
-		a = i;
-		b = (n - a) / 2;
-		total += comb(a+b, b);
-		i += 2;
-	}
+	cout << dp[n] << endl;
 
-	total %= 10007;
-
-	cout << total << endl;
 }
